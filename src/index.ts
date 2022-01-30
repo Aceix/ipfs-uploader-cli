e07b275a0fa629bdd4f6bd006d365e1a8369bec7
Kwesi Frempong-Smart <aceixsmartx@gmail.com>
2022-01-30 20:52:14 +0000

    feat: improve description

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,11 @@ const program = new Command()
 
 program
   .name('ipfs-uploader')
-  .description('Basic CLI tool for upload files to IPFS and storing CID in blockchain')
+  .description('\
+    Basic CLI tool for upload files to IPFS and storing CID in blockchain\n\
+    Ensure the ff env vars are set: \
+    WEB3_PROVIDER, WEB3_ACCOUNT, FILE_STORE_SMART_CONTRACT_ADDRESS, IPFS_URL\
+  ')
   .version('0.0.1')
   .argument('<filePath>', 'Path to file to upload')
   .action(uploadAction)
